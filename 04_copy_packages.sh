36a5ec6a6d1cf66198f8d2c5279f97d54d54f544
builder <builder>
2019-08-12 00:54:56 -0600

    Fix packages path

--- 04_copy_packages.sh
+++ 04_copy_packages.sh
@@ -3,7 +3,7 @@
 ARCH=$(sysctl -n hw.machine)
 LISTE=$(mktemp /tmp/filelist.XXXXXXXXXXXXXXXXXX)
 
-find /build/data/packages/$ARCH/ftp -newer /build/data/packages/$ARCH/all/SHA256 -type f > "${LISTE}"
+find /home/packages/$ARCH/ftp -newer /home/packages/$ARCH/all/SHA256 -type f > "${LISTE}"
 
 doas mkdir -p /mnt/packages-stable/$ARCH/ftp/
 doas rsync --no-relative --files-from="${LISTE}" -av / /mnt/packages-stable/$ARCH/ftp/
