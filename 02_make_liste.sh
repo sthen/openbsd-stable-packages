677c0e22c01e77040d4a8966488ba346ecfdf25b
builder <builder>
2019-08-12 00:12:42 -0600

    produce full list from cvs rdiff

--- 02_make_liste.sh
+++ 02_make_liste.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 
 DESTLIST=/home/builder/fulllist
+TMPLIST=config/tmppkgliste
 
+rm -f $TMPLIST
 rm -f $DESTLIST
 
-for port in $(cat /home/builder/pkgliste)
+# make the list of changed packages (excluding firmwares and folders like "lang/php/")
+# grep only keep ports where the makefile changed
+# sed will format display
+# awk removes folder categories
+cvs -d /cvs rdiff -s  -r OPENBSD_6_5_BASE -r OPENBSD_6_5 ports/ 2>/dev/null | \
+	grep -E '/Makefile(.inc)? ' | \
+	sed -E 's,^File ports/(.*)/Makefile(.inc)? .*,\1,' | \
+	grep -v "^sysutils/firmware" | \
+	awk '{
+		if(NR==1) {
+			line=$0
+		} else {
+			if(index($0,line"/")==0) {
+				print line
+			}
+			line=$0
+		}
+	}
+	END {
+		print line
+	}' > $TMPLIST
+
+
+for port in $(cat $TMPLIST)
 do
 	printf "SELECT DISTINCT fullpkgpath from ports where fullpkgpath like '%s' or  fullpkgpath like '%s,%%'" "$port"  "$port" | sqlite3 /usr/local/share/sqlports >> $DESTLIST
 done
