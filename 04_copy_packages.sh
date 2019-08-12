7dc8594b3784822b6f99364b113bf5aa1739d68f
builder <builder>
2019-08-12 00:14:51 -0600

    auto detect architecture for pkg path

--- 04_copy_packages.sh
+++ 04_copy_packages.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+ARCH=$(sysctl -n hw.machine)
 LISTE=$(mktemp /tmp/filelist.XXXXXXXXXXXXXXXXXX)
 
-find /build/data/packages/amd64/ftp -newer /build/data/packages/amd64/all/SHA256 -type f > "${LISTE}"
+find /build/data/packages/$ARCH/ftp -newer /build/data/packages/$ARCH/all/SHA256 -type f > "${LISTE}"
 
-doas mkdir -p /mnt/packages-stable/amd64/ftp/
-doas rsync --no-relative --files-from="${LISTE}" -av / /mnt/packages-stable/amd64/ftp/
+doas mkdir -p /mnt/packages-stable/$ARCH/ftp/
+doas rsync --no-relative --files-from="${LISTE}" -av / /mnt/packages-stable/$ARCH/ftp/
 
 rm "${LISTE}"
