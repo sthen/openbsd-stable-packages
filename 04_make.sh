8bb5fa09588dfd6dac9b9e6cbe426a6020b22045
builder <builder>
2019-08-13 08:22:38 -0600

    use portslogger and simplify logic

---
+++ 04_make.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x
+set -e
+
+ARCH=$(sysctl -n hw.machine)
+LOGDIR="/mnt/logs-stable/${ARCH}/$(date +%Y%m%d-%H%M)"
+
+doas mkdir -p $LOGDIR
+
+cd /home/ports
+
+
+make SUBDIRLIST=~/fulllist BULK=yes package 2>&1 | doas /home/ports/infrastructure/bin/portslogger $LOGDIR
+
+
+for logs in /mnt/logs-stable/${ARCH}/*tar.gz
+do
+	FOLDER=$(echo $logs | sed 's/\.tar\.gz//')
+	test -d "$FOLDER" && doas rm -fr "$FOLDER"
+done
+
+# compress logs
+doas tar czvf "${LOGDIR}.tar.gz" "${LOGDIR}"
