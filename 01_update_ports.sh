746f8e2e40a18623cc318ee0e010b924c29ac06e
builder <builder>
2019-08-21 08:20:37 -0600

    allow parameter script 01 to update one folder

--- 01_update_ports.sh
+++ 01_update_ports.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 export CVSREADONLYFS=1
-cd /home/ports
+
+# following command allows using $0 /www/nextcloud for quickly updating one folder
+cd /home/ports${1}
 cvs -d /cvs -q up -Pd -A -r OPENBSD_6_5
