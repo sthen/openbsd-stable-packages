c85f236ec69d6690eeb5406c151fd5f6ec23c61b
builder <builder>
2019-08-23 14:38:43 -0600

    prevent empty lines

--- 05_copy_packages.sh
+++ 05_copy_packages.sh
@@ -4,7 +4,7 @@ set -e
 ARCH=$(machine -a)
 LISTE=$(mktemp /tmp/filelist.XXXXXXXXXXXXXXXXXX)
 
-make -C /home/ports SUBDIRLIST=~/fulllist show=PKGNAMES | grep -v "^=" | tr ' ' '\n' | sed 's,$,\.tgz,' | sort | uniq > "${LISTE}"
+make -C /home/ports SUBDIRLIST=~/fulllist show=PKGNAMES | grep -v "^=" | grep ^. | tr ' ' '\n' | sed 's,$,\.tgz,' | sort | uniq > "${LISTE}"
 
 doas mkdir -p /mnt/packages-stable/$ARCH/ftp/
 doas rsync --files-from="${LISTE}" -av /home/packages/$ARCH/ftp/ /mnt/packages-stable/$ARCH/ftp/ | tee config/rsync_output
