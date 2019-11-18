28c09c4db7925c515803e58aceaa46daddad3608
Solene Rapenne <solene@perso.pw>
2019-11-18 11:16:45 +0100

    fixing 02 script filename in cron.sh

--- cron.sh
+++ cron.sh
@@ -11,7 +11,7 @@ LINES=$(./01_update_ports.sh | wc -l | awk '{ print $1 }')
 if [ "$LINES" -ne 0 ]
 then
 	set -x
-    ./02_make_list.sh
+	./02_make_liste.sh
 	set -e
 	./03_clean_packages.sh
 	./04_make.sh
