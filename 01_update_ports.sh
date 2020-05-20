f25288a5bf6715d4cf2a4075be9f11d0d7b9b933
Solene Rapenne <solene@perso.pw>
2020-05-20 15:13:29 +0200

    bump to 6.7

--- 01_update_ports.sh
+++ 01_update_ports.sh
@@ -4,4 +4,4 @@ export CVSREADONLYFS=1
 
 # following command allows using $0 /www/nextcloud for quickly updating one folder
 cd /home/ports${1}
-cvs -d /cvs -q up -Pd -A -r OPENBSD_6_6
+cvs -d /cvs -q up -Pd -A -r OPENBSD_6_7
