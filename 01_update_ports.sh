aa8e50e61381d99f1b15f55d6ee4a65519de83c0
Solene Rapenne <solene@perso.pw>
2020-05-21 13:02:26 +0200

    rework how updating ports can be done with parameters

--- 01_update_ports.sh
+++ 01_update_ports.sh
@@ -2,6 +2,6 @@
 
 export CVSREADONLYFS=1
 
-# following command allows using $0 /www/nextcloud for quickly updating one folder
-cd /home/ports${1}
-cvs -d /cvs -q up -Pd -A -r OPENBSD_6_7
+# pkgpaths can be passed as parameter to only update specific folders
+cd /home/ports
+cvs -d /cvs -q up -Pd -A -r OPENBSD_6_7 $*
