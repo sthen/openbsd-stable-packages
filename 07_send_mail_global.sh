a9ad30cbf440bcddea899061c17963a8b6592370
builder <builder>
2020-05-21 08:03:51 -0600

    centralized building scripts

---
+++ 07_send_mail_global.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+ARCHS="amd64 i386 arm64"
+
+# fetch rsync output files
+for arch in $ARCHS
+do
+	REMOTE=builder@${arch}-stable.ports.openbsd.org
+	scp $REMOTE:scripts/config/rsync_output /tmp/${arch}_output.txt
+done
+
+# generate some build stats for the mail
+{
+        printf "Hello. New package(s) to sign.\n\n---\n"
+
+	# count number of packages per arch
+	for arch in $ARCHS
+	do
+		printf "%i (+%i -debug packages) packages for %s\n" \
+	"$(grep tgz$ /tmp/${arch}_rsync.txt | grep -v ^debug- | sort | uniq | wc -l | awk '{ print $1 }')" \
+	"$(grep tgz$ /tmp/${arch}_rsync.txt | grep ^debug- | sort | uniq | wc -l | awk '{ print $1 }')" \
+	"$arch"
+	done
+        printf "---\n"
+
+	# list every file built per arch with the arch as a beginning of lines
+	for arch in $ARCHS
+	do
+		grep tgz$ /tmp/${arch}_rsync.txt  | sort | uniq | sed "s,^,$arch	,"
+	done
+
+} | mail -s "stable packages to sign" -r "Stable <solene@openbsd.org>" solene@openbsd.org sthen@openbsd.org pea@openbsd.org naddy@openbsd.org 
+
+# clear the remotes rsync output file
+for arch in $ARCHS
+do
+	REMOTE=builder@${arch}-stable.ports.openbsd.org
+	ssh $REMOTE "rm /home/builder/scripts/config/rsync_output"
+done
---
+++ 10_start_remote.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PATHLIST=$(cut -d '/' -f 1-2 config/tmppkgliste | sort | uniq | tr '\n' ' ')
+
+for arch in amd64 i386 arm64; do
+	REMOTE=builder@${arch}-stable.ports.openbsd.org
+	scp ~/fulllist ${REMOTE}:
+	ssh ${REMOTE} "cd scripts && ./01_update_ports.sh $PATHLIST && ./03_clean_packages.sh && ./04_make.sh && ./05_copy_packages.sh" &
+done
+
+wait
+
+echo "finished"
