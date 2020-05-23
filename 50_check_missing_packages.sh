020229b67002d0a93672fbfe70ef7893f77c68a9
builder <builder>
2020-05-23 13:36:58 -0600

    Add a script to find about packages that may be rebuilt

---
+++ 50_check_missing_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export CVSREADONLYFS=1
+
+TMPLIST=config/tmppkgliste
+TMPFILE=$(mktemp /tmp/file.XXXXXXXXXXXXXXXXXXXXXXXX)
+
+
+for port in $(cat $TMPLIST)
+do
+	for dep in $(printf "SELECT DISTINCT fullpkgpath from ports where fullpkgpath like '%s' and not pkgspec LIKE '%%*' ;" "$port" | sqlite3 /usr/local/share/sqlports)
+	do
+		show-reverse-deps "$dep" | tee -a $TMPFILE
+	done
+done
+
+echo "pkgpath that should be included or excluded"
+cat config/{include,exclude}.txt $TMPFILE | sort | uniq
