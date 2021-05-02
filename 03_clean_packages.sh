f67c1ad4d3a809852c563f071d0cf0ea27539db6
Solene Rapenne <solene@perso.pw>
2021-05-02 20:08:46 +0200

    install ccache

--- 03_clean_packages.sh
+++ 03_clean_packages.sh
@@ -2,6 +2,6 @@
 
 doas pkg_delete -X intel-firmware vmm-firmware
 
-doas env TRUSTED_PKG_PATH=/home/packages/$(arch -s)/all/ pkg_add rsync-- sqlports-- git--
+doas env TRUSTED_PKG_PATH=/home/packages/$(arch -s)/all/ pkg_add rsync-- sqlports-- git-- ccache
 
 test -d /build/tmp/pobj && doas rm -fr /build/tmp/pobj/*
