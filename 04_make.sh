a2b5cd5fac7e3fb2dbfb9d04a4a91b1c975df6ab
Solene Rapenne <solene@perso.pw>
2022-11-27 11:32:18 +0100

    enable quirks caching

--- 04_make.sh
+++ 04_make.sh
@@ -11,6 +11,7 @@ cd /home/ports
 
 
 env REPORT_PROBLEM=true SUBDIRLIST=~/fulllist BULK=yes make package 2>&1 | doas /home/ports/infrastructure/bin/portslogger $LOGDIR
+cd /home/ports/devel/quirks && make clean=all && make REPORT_PROBLE=true BULK=yes package | doas /home/ports/infrastructure/bin/portslogger $LOGDIR
 
 
 for logs in /mnt/logs-stable/${ARCH}/*tar.gz
