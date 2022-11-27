72a5c92070b7b2d24c8b32aa97ce9432bb04ddee
Solene Rapenne <solene@perso.pw>
2022-11-27 11:33:26 +0100

    use openrsync instead of scp

--- 10_start_remote.sh
+++ 10_start_remote.sh
@@ -9,7 +9,7 @@ env SUBDIRLIST=~/fulllist BULK=yes make fetch
 for arch in sparc64 amd64 i386 arm64; do
 	REMOTE=builder@${arch}-stable.ports.openbsd.org
 	scp config/*clude.txt ${REMOTE}:scripts/config/
-	scp ~/fulllist ${REMOTE}:
+	openrsync -av ~/fulllist ${REMOTE}:
 	ssh ${REMOTE} "cd scripts && ./01_update_ports.sh $PATHLIST && ./03_clean_packages.sh && ./04_make.sh && ./05_copy_packages.sh" &
 done
 
