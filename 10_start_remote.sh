7a029758fdb401f7058835248d354e28af371feb
Stuart Henderson <stu@spacehopper.org>
2025-04-22 21:18:29 +0100

    update full ports tree on builders, not just the dirs found from parsing rdiff

--- 10_start_remote.sh
+++ 10_start_remote.sh
@@ -10,8 +10,9 @@ for arch in amd64 i386; do
 	REMOTE=builder@${arch}-stable.ports.openbsd.org
 	scp config/*clude.txt ${REMOTE}:scripts/config/
 	scp ~/fulllist ${REMOTE}:
-	ssh ${REMOTE} "cd scripts && ./01_update_ports.sh $PATHLIST &&
-		./03_clean_packages.sh && ./04_make.sh && ./05_copy_packages.sh" &
+	ssh ${REMOTE} "cd scripts && ./01_update_ports.sh &&
+		./03_clean_packages.sh && ./04_make.sh &&
+		./05_copy_packages.sh" &
 done
 
 wait
