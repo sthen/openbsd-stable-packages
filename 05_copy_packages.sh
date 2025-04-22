3f97c3f3caf859eb16a02f9743c95675b884a75e
Stuart Henderson <stu@spacehopper.org>
2025-04-22 19:57:05 +0100

    tidy

--- 05_copy_packages.sh
+++ 05_copy_packages.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/ksh -e
 
-set -e
 ARCH=$(machine -a)
-LISTE=$(mktemp /tmp/filelist.XXXXXXXXXXXXXXXXXX)
+FILELIST=$(mktemp /tmp/filelist.XXXXXXXX)
 
-env SUBDIRLIST=~/fulllist show=PKGNAMES make -C /home/ports | grep -v "^=" | grep ^. | tr ' ' '\n' | sed 's,$,\.tgz,' | sort | uniq > "${LISTE}"
+SUBDIRLIST=~/fulllist make -C /home/ports show=PKGNAMES |
+	grep -v "^=" | grep ^. | tr ' ' '\n' | sed 's,$,\.tgz,' |
+	sort | uniq > $FILELIST
 
 doas mkdir -p /mnt/packages-stable/$ARCH/ftp/
-doas rsync --files-from="${LISTE}" -av /home/packages/$ARCH/ftp/ /mnt/packages-stable/$ARCH/ftp/ | tee -a config/rsync_output
+doas rsync --files-from=$FILELIST -av /home/packages/$ARCH/ftp/ \
+	/mnt/packages-stable/$ARCH/ftp/ | tee -a config/rsync_output
 
-
-rm "${LISTE}"
+rm $FILELIST
