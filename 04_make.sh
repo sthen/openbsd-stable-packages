a863ddc5e3b5f73a2db543b9f6f54ddf0a876f14
Stuart Henderson <stu@spacehopper.org>
2025-10-19 14:01:36 +0100

    use REPORT_PROBLEM_LOGFILE to write failed build paths to a file in logdir don't switch uid for writing logs actually remove old uncompressed logs

--- 04_make.sh
+++ 04_make.sh
@@ -2,15 +2,16 @@
 
 ARCH=$(machine -a)
 LOGDIR="/mnt/logs-stable/${ARCH}/$(date +%Y%m%d-%H%M)"
+mkdir -p $LOGDIR
+chmod g+w $LOGDIR
 
-doas mkdir -p $LOGDIR
+doas env PKG_PATH=/home/packages/$ARCH/all pkg_add -r -D unsigned glib2
 
 cd /home/ports
 
 # export REPORT_PROBLEM=/home/builder/scripts/error_handler.sh
-export REPORT_PROBLEM=true
-
-doas env PKG_PATH=/home/packages/$ARCH/all pkg_add -r -D unsigned glib2
+#export REPORT_PROBLEM=true
+export REPORT_PROBLEM_LOGFILE=$LOGDIR/ZZ-build-problems
 
 SUBDIRLIST=~/fulllist make package \
 	BULK=yes PKG_PATH=/var/empty FETCH_PACKAGES= 2>&1 | \
@@ -19,15 +20,10 @@ SUBDIRLIST=~/fulllist make package \
 for i in devel/quirks databases/updatedb; do
 	cd /home/ports/$i
 	make clean=all
-	make REPORT_PROBLEM=~/scripts/error_handler.sh BULK=yes package | \
-		doas /home/ports/infrastructure/bin/portslogger $LOGDIR
+	make REPORT_PROBLEM=~/scripts/error_handler.sh BULK=yes package 2>&1 | \
+		/home/ports/infrastructure/bin/portslogger $LOGDIR
 done
 
-for logs in /mnt/logs-stable/${ARCH}/*tar.gz
-do
-	DIR=${logs%.tar.gz}
-	test -d "$DIR" && doas rm -fr "$FOLDER"
-done
-
-# compress logs
-doas tar czvf "${LOGDIR}.tar.gz" "${LOGDIR}"
+# compress and remove old logs
+tar czvf "${LOGDIR}.tar.gz" "${LOGDIR}"
+find /mnt/logs-stable/${ARCH} -type d -mindepth 1 -maxdepth 1 -mtime +21 -exec {} +
