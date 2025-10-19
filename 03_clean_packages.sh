e58daefb8fbe03235629d1a8a7a848619bac7bf8
Stuart Henderson <stu@spacehopper.org>
2025-10-19 14:05:26 +0100

    don't clean build dir as root, _pbuild is enough

--- 03_clean_packages.sh
+++ 03_clean_packages.sh
@@ -2,6 +2,6 @@
 
 doas pkg_delete -X intel-firmware vmm-firmware
 doas env TRUSTED_PKG_PATH=/home/packages/$(arch -s)/all/ pkg_add rsync-- sqlports-- git-- ccache
-test -d /build/tmp/pobj && doas rm -fr /build/tmp/pobj/*
+doas -u _pbuild rm -fr /build/tmp/pobj
 doas mkdir -p /build/tmp/pobj
 doas chown _pbuild /build/tmp/pobj
