0b441229aca1b6fad2f2ecef0e1bdb008c3acdc4
Stuart Henderson <stu@spacehopper.org>
2025-10-19 06:10:43 +0100

    cope with cleaned /build fs

--- 03_clean_packages.sh
+++ 03_clean_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 doas pkg_delete -X intel-firmware vmm-firmware
-
 doas env TRUSTED_PKG_PATH=/home/packages/$(arch -s)/all/ pkg_add rsync-- sqlports-- git-- ccache
-
 test -d /build/tmp/pobj && doas rm -fr /build/tmp/pobj/*
+doas mkdir -p /build/tmp/pobj
+doas chown _pbuild /build/tmp/pobj
