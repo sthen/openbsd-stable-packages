37cac796e1cced1fcf0b3cbb3e22b84899bec570
Stuart Henderson <stu@spacehopper.org>
2025-10-20 19:26:18 +0100

    move old logs from logs-stable/arch/xx to logs-stable/old/arch/x, use logs-stable/arch for current logs various shell tweaks trim path in tarred logs

--- 04_make.sh
+++ 04_make.sh
@@ -1,31 +1,37 @@
 #!/bin/ksh -e
 
-ARCH=$(machine -a)
-LOGDIR="/mnt/logs-stable/${ARCH}/$(date +%Y%m%d-%H%M)"
-mkdir -p $LOGDIR
-chmod g+w $LOGDIR
+ARCH="$(machine -a)"
+LOGDIR="/mnt/logs-stable/${ARCH}"
+mkdir -p "${LOGDIR}"
+chmod g+w "${LOGDIR}"
 
-doas env PKG_PATH=/home/packages/$ARCH/all pkg_add -r -D unsigned glib2
+doas env PKG_PATH="/home/packages/${ARCH}/all" pkg_add -r -D unsigned glib2
 
 cd /home/ports
 
 # export REPORT_PROBLEM=/home/builder/scripts/error_handler.sh
 #export REPORT_PROBLEM=true
-export REPORT_PROBLEM_LOGFILE=$LOGDIR/ZZ-build-problems
+export REPORT_PROBLEM_LOGFILE="${LOGDIR}/ZZ-build-problems"
 
 SUBDIRLIST=~/fulllist make package \
 	BULK=yes PKG_PATH=/var/empty FETCH_PACKAGES= 2>&1 | \
-	/home/ports/infrastructure/bin/portslogger $LOGDIR > /dev/null
+	/home/ports/infrastructure/bin/portslogger "${LOGDIR}" > /dev/null
 
 for i in devel/quirks databases/updatedb; do
-	cd /home/ports/$i
+	cd "/home/ports/$i"
 	make clean=all
 	make REPORT_PROBLEM=~/scripts/error_handler.sh BULK=yes package 2>&1 | \
-		/home/ports/infrastructure/bin/portslogger $LOGDIR
+		/home/ports/infrastructure/bin/portslogger "${LOGDIR}"
 done
 
-cat $LOGDIR/ZZ-build-problems
+if [[ -r ${LOGDIR}/ZZ-build-problems ]]; then
+	echo build problems:
+	cat "${LOGDIR}/ZZ-build-problems"
+fi
 
-# compress and remove old logs
-tar czvf "${LOGDIR}.tar.gz" "${LOGDIR}"
-find /mnt/logs-stable/${ARCH} -type d -mindepth 1 -maxdepth 1 -mtime +21 -exec {} +
+cd "/mnt/logs-stable/old/${ARCH}"
+LOGSUBDIR="$(date +%Y%m%d-%H%M)"
+mv "${LOGDIR}" "$LOGSUBDIR"
+tar czvf "${LOGSUBDIR}.tar.gz" "${LOGSUBDIR}"
+find "/mnt/logs-stable/old/${ARCH}" -type d -mindepth 1 -maxdepth 1 -mtime +21 -exec {} +
+
