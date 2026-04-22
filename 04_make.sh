48fc99841cf2655b0019d27fa713cafd344d07c8
Stuart Henderson <stu@spacehopper.org>
2026-04-22 14:48:59 +0100

    config/include.txt was the wrong way to build sqlports, as it needs cleaning first

--- 04_make.sh
+++ 04_make.sh
@@ -10,14 +10,14 @@ doas env PKG_PATH="/home/packages/${ARCH}/all" pkg_add -r -D unsigned glib2
 cd /home/ports
 
 # export REPORT_PROBLEM=/home/builder/scripts/error_handler.sh
-#export REPORT_PROBLEM=true
+# export REPORT_PROBLEM=true
 export REPORT_PROBLEM_LOGFILE="${LOGDIR}/ZZ-build-problems"
 
 SUBDIRLIST=~/fulllist make package \
 	BULK=yes PKG_PATH=/var/empty FETCH_PACKAGES= 2>&1 | \
 	/home/ports/infrastructure/bin/portslogger "${LOGDIR}" > /dev/null
 
-for i in devel/quirks databases/updatedb; do
+for i in databases/sqlports devel/quirks databases/updatedb; do
 	cd "/home/ports/$i"
 	make clean=all
 	make REPORT_PROBLEM=~/scripts/error_handler.sh BULK=yes package 2>&1 | \
